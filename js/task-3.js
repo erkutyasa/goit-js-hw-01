23c3f74e36a6e89600922f94a20a4b68115c56d4
Erkut <erkt.ysa@gmail.com>
2024-07-10 23:07:13 +0300

    jshomeworkend

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,14 @@
+
 function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content)
-    const paddingWidth = parseFloat(padding)
-    const borderWidth = parseFloat(border)
-
-    const totalWidth = contentWidth + paddingWidth + borderWidth
-    return totalWidth
-}
-
-console.log(getElementWidth("50px", "8px", "4px")); // 62
-console.log(getElementWidth("60px", "12px", "8.5px")); // 80.5
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+    
+    content = parseFloat(content);
+    padding = parseFloat(padding);
+    border =  parseFloat(border);
+  
+    return Math.round(content + (padding * 2) + (border * 2));
+  }
+  
+  console.log(getElementWidth("50px", "8px", "4px")); // 74
+  console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+  console.log(getElementWidth("200px", "0px", "0px")); // 200
+  
